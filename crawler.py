f021ea4721ff776628f18289180add1d954a4737
Aleksei Samburov <aesamburov@gmail.com>
2018-05-04 16:29:46 +0300

    Static analysis

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@ from tldextract import extract
 
 from typing import Set, List
 
-ENTRY_POINT = "http://www.python.org"
+ENTRY_POINT = "https://www.jetbrains.com"
 
 
 def check_url(url: str) -> bool:
@@ -31,7 +31,6 @@ def crawl_page(driver: WebDriver, url: str, available_links: Set[str]) -> Set[st
     driver.get(url)
     available_links.add(url)
     actions = get_actions(driver)
-    print(actions)
     for action in actions:
         if action in available_links:
             continue
@@ -40,8 +39,41 @@ def crawl_page(driver: WebDriver, url: str, available_links: Set[str]) -> Set[st
     return available_links
 
 
+def build_site_maps(driver: WebDriver, entry_point: str):
+    site_maps = dict()
+    role = 'guest'
+    while role != 'no':
+        site_map = build_site_map(driver, entry_point)
+        site_maps[role] = site_map
+        driver.get(entry_point)
+        role = input("Please login and type user role to continue parsing as another user. Type 'no' to finish preparation phase and check for vulnerabilities.")
+    return site_maps
+
+
+def build_site_map(driver: WebDriver, entry_point: str):
+    site_map = set()
+    crawl_page(driver, entry_point, all_links)
+    driver.delete_all_cookies()
+    return site_map
+
+
+def check_for_escalation(driver: WebDriver, site_maps):
+    for role in site_maps.keys():
+        driver.get(entry_point)
+        input('Please login as {} and press ENTER'.format(role))
+        for other_role in site_maps.keys():
+            if other_role == role:
+                continue
+            for link in site_maps[other_role]:
+                if link not in site_maps[role]:
+                    driver.get(link)
+                    if '404' in driver.page_source:
+                        print('User {} can access {} via direct link, but cannot access this page via interface'.format(role, link))
+ 
+
+
 firefox_driver = webdriver.Firefox()
-all_links = set()
-crawl_page(firefox_driver, ENTRY_POINT, all_links)
+site_maps = build_site_maps(firefox_driver, ENTRY_POINT)
+check_for_escalation(firefox_driver, site_maps)
 firefox_driver.close()
 print(all_links)
